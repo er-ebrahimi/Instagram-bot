251cb08db7b94effb9a9951ab411ee48246a4aa0
Erfan Ebrahimi <erfan.es1381@gmail.com>
2023-03-18 15:37:52 +0330

    Removing files, Getting info
    Removing files from system,
    Getting amount of posts,
    Fix uploading medias with comments
    Fix uploading try exception

--- DownloadFileInstagrapi.py
+++ DownloadFileInstagrapi.py
@@ -17,74 +17,103 @@ user_id = cl.user_id_from_username(your_dest)
 
 print(f"user id catched {user_id}")
 
+user_info = cl.user_info(user_id)
+
+print(f"user info: amount  catched {user_info.media_count}")
+
+def remove_file(path: str):
+    if type(path) is list:
+        for p in path:
+            print(f"removing file {p}")
+            os.remove(p)
+            print('file removed')
+    else:
+        print(f"removing file {path}")
+        os.remove(path)
+        print('file removed')
+
 def download_post():
     try:
-        media_list = cl.user_medias(user_id,3)
+        posts = {}
+        media_list = cl.user_medias(user_id,2)
         print(f'media list catched its size is {len(media_list)}')
-
         for media in media_list:
             print(f'media.pk is {media.pk}')
             if media.media_type == 1 :
-                cl.photo_download(media.pk, folder=path)
+                posts[media.pk]=cl.photo_download(media.pk, folder=path)
                 print("photo downloaded")
             elif media.media_type == 2 and media.product_type == 'feed':
-                cl.video_download(media.pk, folder=path)
+                posts[media.pk]=cl.video_download(media.pk, folder=path)
                 print("video downloaded")
             elif media.media_type == 2 and media.product_type == 'igtv':
-                cl.igtv_download(media.pk, folder=path)
+                posts[media.pk]=cl.igtv_download(media.pk, folder=path)
                 print("igtv downloaded")
             elif media.media_type == 2 and media.product_type == 'clips':
-                cl.clip_download(media.pk, folder=path)
+                posts[media.pk]=cl.clip_download(media.pk, folder=path)
                 print("clip downloaded")
             elif media.media_type == 8 :
-                cl.album_download(media.pk, folder=path)
+                posts[media.pk]=cl.album_download(media.pk, folder=path)
                 print("album downloaded")
             else:
                 print("couldn't find the type of media")
             
-        for data in os.listdir(path):
-            print(f'media.pk is {media.pk}')
-            index = 0
-            # index = findMedia(data,media_list)
-            upload_path = os.path.join(path,data)
-            if type(index) != False:
-                if media.media_type == 1 :
-                    print(f"photo uploading in path {upload_path}")
-                    cl.photo_upload(path=upload_path,caption= media_list[index].caption_text)
-                    print("photo uploaded")
-                elif media.media_type == 2 and media.product_type == 'feed':
-                    print(f"media uploading in path {upload_path}")
-                    cl.video_upload(path=upload_path,caption= media_list[index].caption_text)
-                    print("video uploaded")
-                elif media.media_type == 2 and media.product_type == 'igtv':
-                    print(f"igtv uploading in path {upload_path}")
-                    cl.igtv_upload( path=upload_path,caption= media_list[index].caption_text)
-                    print("igtv uploaded")
-                elif media.media_type == 2 and media.product_type == 'clips':
-                    print(f"clip uploading in path {upload_path}")
-                    cl.clip_upload( path=upload_path,caption= media_list[index].caption_text)
-                    print("clip uploaded")
-                elif media.media_type == 8 :
-                    print(f"album uploading in path {upload_path}")
-                    cl.album_upload( paths=upload_path,caption= media_list[index].caption_text)
-                    print("album uploaded")
-                else:
-                    print("couldn't find the type of media")
-            else:
-                print(f'type of index wasnt int {index}')
-
-        print('finished')
     except Exception as e:
         print(f'Exception occuerd: {e}')
-        ques = input('Do you want to start downloading again:(Y/N) ')
+        ques = input('Do you want to download again or start uploading:(Y/N) ')
         if ques == 'Y' or ques == 'y':
             download_post()
         elif ques == 'N' or ques == 'n':
-            exit()
+            pass
+    while True:
+        try:
+            for media in media_list:
+                if media.media_type == 1 :
+                    print(f"photo uploading in path {posts[media.pk]}")
+                    cl.photo_upload(path=posts[media.pk],caption= media.caption_text)
+                    print("photo uploaded")
 
+                elif media.media_type == 2 and media.product_type == 'feed':
+                    print(f"media uploading in path {posts[media.pk]}")
+                    cl.video_upload(path=posts[media.pk],caption= media.caption_text)
+                    print("video uploaded")               
 
+                elif media.media_type == 2 and media.product_type == 'igtv':
+                    print(f"igtv uploading in path {posts[media.pk]}")
+                    cl.igtv_upload( path=posts[media.pk],caption= media.caption_text)
+                    print("igtv uploaded")
 
-# download_post()
+                elif media.media_type == 2 and media.product_type == 'clips':
+                    print(f"clip uploading in path {posts[media.pk]}")
+                    cl.clip_upload( path=posts[media.pk],caption= media.caption_text)
+                    print("clip uploaded")
+
+                elif media.media_type == 8 :
+                    print(f"album uploading in path {posts[media.pk]} with this caption {media.caption_text}")
+                    cl.album_upload( paths=posts[media.pk],caption= media.caption_text)
+                    print("album uploaded")
+
+                else:
+                    print("couldn't find the type of media")
+        except Exception as e:
+            print(f'Exception occuerd: {e}')
+            ques = input('Do you want to start uploading again:(Y/N) ')
+            if ques == 'Y' or ques == 'y':
+                continue
+            elif ques == 'N' or ques == 'n':
+                break
+        print('finished')
+        print('start removing files')
+        for i in media_list:
+            remove_file(posts[i.pk])
+        print('files removed')
+        media_list.clear()
+        print('media list cleared')
+        posts.clear()
+        print('posts cleared')
+        break
+        
+
+download_post()
 schedule.every().day.at("01:30").do(download_post)
 # Run the scheduled functions
 while True:
